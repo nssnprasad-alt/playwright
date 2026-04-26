b79600ab52a816d32a594b8eefe8662225383b29
nssnprasad-alt <nssnprasad@gmail.com>
2026-04-26 09:01:29 +0530

    added headless mode

--- tests/windows.spec.js
+++ tests/windows.spec.js
@@ -1,24 +1,24 @@
-import { test, expect, request, chromium } from '@playwright/test';
-test('child window handling', async () => {
+// import { test, expect, request, chromium } from '@playwright/test';
+// test('child window handling', async () => {
 
-    const browser = await chromium.launch({
-        headless: false,
-        slowMo: 1000
-    });
+//     const browser = await chromium.launch({
+//         headless: false,
+//         slowMo: 1000
+//     });
 
-    const context = await browser.newContext();
-    const page = await context.newPage();
+//     const context = await browser.newContext();
+//     const page = await context.newPage();
 
-    await page.goto("https://demoqa.com/browser-windows");
+//     await page.goto("https://demoqa.com/browser-windows");
 
-    const [newPage] = await Promise.all([
-        context.waitForEvent('page'),
-        page.locator('#tabButton').click()
-    ]);
+//     const [newPage] = await Promise.all([
+//         context.waitForEvent('page'),
+//         page.locator('#tabButton').click()
+//     ]);
 
-    await newPage.waitForLoadState();
+//     await newPage.waitForLoadState();
 
-    console.log(await newPage.locator('h1').textContent());
+//     console.log(await newPage.locator('h1').textContent());
 
-    await page.waitForTimeout(5000); // 👈 keep open
-});
+//     await page.waitForTimeout(5000); // 👈 keep open
+// });
